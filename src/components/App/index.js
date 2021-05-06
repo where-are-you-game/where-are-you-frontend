ec8679251d87b28b46c3e26ddce3ee08e2fb6832
eluka22 <eluka22@gmail.com>
2021-05-06 20:52:35 +0900

    Move: App to components

---
+++ src/components/App/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+
+import GlobalStyles from "../../styles";
+import GlobalFonts from "../../styles/fonts";
+import theme from "../../styles/theme";
+import Game from "../Game";
+import Main from "../Main";
+
+const Wrapper = styled.div``;
+
+function App() {
+  return (
+    <Wrapper>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <GlobalFonts />
+        <Router>
+          <Route path="/" exact component={Main} />
+          <Route path="/game/livingroom" component={Game} />
+        </Router>
+      </ThemeProvider>
+    </Wrapper>
+  );
+}
+
+export default App;
