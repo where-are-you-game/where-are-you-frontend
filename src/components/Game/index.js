255deebc6802ed6eb8be4dd71296f81f281cb09c
eluka22 <eluka22@gmail.com>
2021-05-06 20:56:02 +0900

    Add: components

---
+++ src/components/Game/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 800px;
+  height: 600px;
+`;
+
+function index() {
+  return (
+    <Wrapper></Wrapper>
+  );
+}
+
+export default index;
