9ebbd6833601af211c0fcd6d1426dfcbd57f2714
eluka22 <eluka22@gmail.com>
2021-05-07 14:35:37 +0900

    Add: LeftButton and RightButton

---
+++ src/components/Shared/LeftButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import leftButton from "../../assets/common/left_button.png";
+
+const Button = styled(Link)`
+  width: 260px;
+  height: 130px;
+  padding: 40px 0 0 80px;
+  display: inline-block;
+  position: absolute;
+  top: calc(250px + 1rem);
+  left: calc((260px + 3rem) * -1);
+  background: url(${leftButton});
+  font-size: 2rem;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.orange};
+    transform: rotate(-7deg);
+  }
+`;
+
+function LeftButton({ path, text }) {
+  return (
+    <Button to={path}>
+      {text}
+    </Button>
+  );
+}
+
+LeftButton.propTypes = {
+  path: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+export default LeftButton;
---
+++ src/components/Shared/RightButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import rightButton from "../../assets/common/right_button.png";
+
+const Button = styled(Link)`
+  width: 260px;
+  height: 130px;
+  padding: 50px 0 0 80px;
+  display: inline-block;
+  position: absolute;
+  top: calc(250px + 1rem);
+  right: calc((260px + 3rem) * -1);
+  background: url(${rightButton});
+  font-size: 2rem;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.orange};
+    transform: rotate(7deg);
+  }
+`;
+
+function RightButton({ path, text }) {
+  return (
+    <Button to={path}>
+      {text}
+    </Button>
+  );
+}
+
+RightButton.propTypes = {
+  path: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+export default RightButton;
