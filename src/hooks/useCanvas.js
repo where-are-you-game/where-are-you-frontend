7223d6cb0b10f96f907005385eb17df177a15497
eluka22 <eluka22@gmail.com>
2021-05-08 22:50:33 +0900

    Fix: check overlay image on canvas

--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -2,17 +2,16 @@ import { useEffect } from "react";
 
 import { drawImage, clickImage } from "../utils/canvas";
 
-const useCanvas = (canvasRef, objects, showPuzzle) => {
+const useCanvas = (canvasRef, images, showPuzzle) => {
   return useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
-    for (const object in objects) {
-      const image = objects[object];
+    for (const image of images) {
       drawImage(context, image.src, image.x, image.y, image.width, image.height);
     }
 
-    clickImage(canvas, objects, showPuzzle);
+    clickImage(canvas, context, images, showPuzzle);
   }, []);
 };
 
--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -5,6 +5,28 @@ const getMousePosition = (canvas, event) => {
   return { mouseX, mouseY };
 };
 
+const isMouseXOnImage = (mouseX, imageX, imageWidth) => {
+  return mouseX > imageX && mouseX < imageX + imageWidth;
+};
+
+const isMouseYOnImage = (mouseY, imageY, imageHeight) => {
+  return mouseY > imageY && mouseY < imageY + imageHeight;
+};
+
+const getOverlayImage = (mouseX, mouseY, currentImage, overlayImages, allImages) => {
+  let overlayImage = currentImage;
+
+  allImages.forEach((image) => {
+    if (overlayImages.includes(image.object)) {
+      if (isMouseXOnImage(mouseX, image.x, image.width) && isMouseYOnImage(mouseY, image.y, image.height)) {
+        overlayImage = image;
+      }
+    }
+  });
+
+  return overlayImage;
+};
+
 export const drawImage = (context, img, x, y, width, height) => {
   const image = new Image();
   image.src = img;
@@ -13,18 +35,37 @@ export const drawImage = (context, img, x, y, width, height) => {
   };
 };
 
-export const clickImage = (canvas, objects, callback) => {
+export const removeImage = (context, x, y, width, height) => {
+  context.clearRect(x, y, width, height);
+};
+
+export const clickImage = (canvas, context, images, callback) => {
   canvas.addEventListener("click", (event) => {
     const { mouseX, mouseY } = getMousePosition(canvas, event);
 
-    for (const image in objects) {
-      const object = objects[image];
+    for (const image of images) {
+      let currentImage = image;
 
       if (
-        (mouseX > object.x && mouseX < object.x + object.width)
-        && (mouseY > object.y && mouseY < object.y + object.height)
+        isMouseXOnImage(mouseX, image.x, image.width)
+        && isMouseYOnImage(mouseY, image.y, image.height)
       ) {
-        callback(object);
+
+        if (image.overlayObjects) {
+          currentImage = getOverlayImage(mouseX, mouseY, currentImage, image.overlayObjects, images);
+        }
+
+        if (currentImage.srcAfter) {
+          const originSrc = currentImage.src;
+          const afterSrc = currentImage.srcAfter;
+          currentImage.src = afterSrc;
+          currentImage.srcAfter = originSrc;
+
+          removeImage(context, currentImage.x, currentImage.y, currentImage.width, currentImage.height);
+          drawImage(context, afterSrc, currentImage.x, currentImage.y, currentImage.width, currentImage.height);
+        }
+
+        callback(currentImage);
         return;
       }
     }
