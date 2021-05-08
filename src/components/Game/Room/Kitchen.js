ea511ef2c9e9cd35cb30efb618f561ae79786de6
eluka22 <eluka22@gmail.com>
2021-05-08 22:52:55 +0900

    Add: canvas in Kitchen

--- src/components/Game/Room/Kitchen.js
+++ src/components/Game/Room/Kitchen.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function Kitchen() {
+import PropTypes from "prop-types";
+
+import kitchenObjects from "../../../constants/kitchen";
+import useCanvas from "../../../hooks/useCanvas";
+import RightButton from "../../Shared/RightButton";
+
+function Kitchen({ showPuzzle }) {
+  const canvasRef = useRef();
+
+  useCanvas(canvasRef, kitchenObjects, showPuzzle);
+
   return (
-    <div>
-      Kitchen
-    </div>
+    <>
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+      />
+      <RightButton path="/game/livingroom" text="거실" />
+    </>
   );
 }
 
+Kitchen.propTypes = {
+  showPuzzle: PropTypes.func.isRequired
+};
+
 export default Kitchen;
+
