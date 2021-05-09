f017b942237b66b5734426721e4063d448975ed8
eluka22 <eluka22@gmail.com>
2021-05-09 09:56:49 +0900

    Add: Menu

---
+++ src/components/Game/Menu.js
@@ -0,0 +1,102 @@
+import React, { useRef } from "react";
+
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import CloseButton from "../Shared/ModalCloseButton";
+
+const Backdrop = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  background: rgba(0, 0, 0, 0.4);
+`;
+
+const Wrapper = styled.div`
+  width: 400px;
+  height: auto;
+  padding: ${({ theme }) => theme.padding.big};
+  position: relative;
+  background: white;
+`;
+
+const Title = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin: 0 0 0.5rem 0;
+    text-align: center;
+  }
+`;
+
+const Button = styled(Link)`
+  width: 200px;
+  padding: 0.5rem;
+  display: inline-block;
+  background: ${({ theme }) => theme.color.background};
+  font-size: 1.3rem;
+  transition: 0.3s all;
+
+  &:hover {
+    background: ${({ theme }) => theme.color.black};
+    color: #ffffff;
+  }
+`;
+
+function Menu({ hideMenu }) {
+  const option = useRef();
+
+  const closeMenu = () => {
+    hideMenu(false);
+  };
+
+  const clickOptionOutside = (event) => {
+    if (!option.current || option.current.contains(event.target)) return;
+    closeMenu();
+  };
+
+  return (
+    <Backdrop onClick={clickOptionOutside}>
+      <Wrapper ref={option}>
+        <CloseButton
+          title="Close"
+          top="-30px"
+          closeModal={closeMenu}
+          color="#f8a507"
+        />
+        <Title>MENU</Title>
+        <List>
+          <li>
+            <Button>메인 화면으로</Button>
+          </li>
+          <li>
+            <Button>처음부터 다시 하기</Button>
+          </li>
+          <li>
+            <Button>리뷰 보러가기</Button>
+          </li>
+        </List>
+      </Wrapper>
+    </Backdrop>
+  );
+}
+
+Menu.propTypes = {
+  hideMenu: PropTypes.func.isRequired
+};
+
+export default Menu;
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
+import configIcon from "../../assets/common/config_icon.png";
+import Menu from "./Menu";
 import Puzzle from "./Puzzle";
 import Room from "./Room";
 import Textbox from "./Textbox";
@@ -37,7 +39,24 @@ const Wrapper = styled.div`
   }
 `;
 
+const MenuButton = styled.button`
+  width: 30px;
+  height: 30px;
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  right: calc((50px + 0.5rem) * -1);
+  background: #ffffff url(${configIcon}) no-repeat center;
+  opacity: 0.4;
+  transition: 0.3s all;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 function Game() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   const puzzleModal = useSelector(state => state.puzzleModal);
   const { room } = useParams();
 
@@ -47,10 +66,15 @@ function Game() {
         <Todolist />
         <Room room={room} />
         <Textbox />
+        <MenuButton
+          type="button"
+          onClick={() => setIsMenuVisible(!isMenuVisible)}
+        />
       </Wrapper>
+      {isMenuVisible
+        && <Menu hideMenu={setIsMenuVisible} />}
       {puzzleModal.isVisible
-        && <Puzzle />
-      }
+        && <Puzzle />}
     </>
   );
 }
