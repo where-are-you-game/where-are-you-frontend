ddc0e89f946f7e4b706069920642e1c78b30b966
eluka22 <eluka22@gmail.com>
2021-05-13 16:40:48 +0900

    Style: separate common button style

--- src/components/Shared/LinkButton.js
+++ src/components/Shared/LinkButton.js
@@ -4,40 +4,10 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { buttonStyle } from "../../styles";
+
 const Button = styled(Link)`
-  width: 200px;
-  height: 60px;
-  margin: 1rem 0 0 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  border: 1px solid ${({ theme, color }) => theme.color[color]};
-  text-align: center;
-  cursor: pointer;
-  transition: all 400ms cubic-bezier(0.77, 0, 0.175, 1);
-
-  &::after {
-    width: 100%;
-    height: 0;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    z-index: -1;
-    background: ${({ theme, color }) => theme.color[color]};
-    content: "";
-    transition: inherit;
-  }
-
-  &:hover {
-    box-shadow: 1px 1px 15px 2px rgba(0, 0, 0, 0.1);
-    color: #ffffff;
-
-    &::after {
-      height: 100%;
-      top: 0;
-    }
-  }
+  ${buttonStyle}
 `;
 
 function LinkButton({ path, title, color }) {
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,5 @@
 import ReseterCSS from "reseter.css";
-import { createGlobalStyle } from "styled-components";
+import { css, createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   ${ReseterCSS};
@@ -25,6 +25,42 @@ const GlobalStyles = createGlobalStyle`
   ul,
   ul li {
     list-style: none;
+  }
+`;
+
+export const buttonStyle = css`
+  width: 200px;
+  height: 60px;
+  margin: 1rem 0 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  border: 1px solid ${({ theme, color }) => theme.color[color]};
+  text-align: center;
+  cursor: pointer;
+  transition: all 400ms cubic-bezier(0.77, 0, 0.175, 1);
+
+  &::after {
+    width: 100%;
+    height: 0;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    background: ${({ theme, color }) => theme.color[color]};
+    content: "";
+    transition: inherit;
+  }
+
+  &:hover {
+    box-shadow: 1px 1px 15px 2px rgba(0, 0, 0, 0.1);
+    color: #ffffff;
+
+    &::after {
+      height: 100%;
+      top: 0;
+    }
   }
 `;
 
