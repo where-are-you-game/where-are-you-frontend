8e1b44cc221993bb3c4aececb3b0383a0ade4205
eluka22 <eluka22@gmail.com>
2021-05-14 16:21:46 +0900

    Update: hide COUNT_CLEARTIME action in logger

--- src/store.js
+++ src/store.js
@@ -2,12 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import { persistStore } from "redux-persist";
 
+import * as types from "./constants/actionTypes";
 import reducer from "./reducers";
 
 const middleware = [];
 
 if (process.env.NODE_ENV !== "production") {
-  middleware.push(createLogger());
+  const logger = createLogger({
+    predicate: (getState, action) => action.type !== types.COUNT_CLEARTIME
+  });
+  middleware.push(logger);
 }
 
 export const store = createStore(reducer, applyMiddleware(...middleware));
