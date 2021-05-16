9e74436db40df98f03f6ea2adbade114396d6efe
eluka22 <eluka22@gmail.com>
2021-05-16 20:35:02 +0900

    Update: review reducer

--- src/reducers/review.js
+++ src/reducers/review.js
@@ -3,30 +3,18 @@ import produce from "immer";
 import * as type from "../constants/actionTypes";
 
 const initialState = {
-  byId: {},
-  allIds: []
+  list: []
 };
 
 const review = (state = initialState, action) => {
   switch (action.type) {
     case type.SAVE_REVIEWS:
       return produce(state, (draft) => {
-        const reviews = {};
-
-        for (let i = 0; i < action.reviews.length; i++) {
-          const review = action.reviews[i];
-          reviews[review._id] = review;
-          draft.allIds.push(review._id);
-        }
-
-        draft.byId = reviews;
+        draft.list = [ ...draft.list, ...action.reviews ];
       });
     case type.SAVE_REVIEW:
       return produce(state, (draft) => {
-        const review = action.review;
-
-        draft.byId[review._id] = review;
-        draft.allIds.unshift(review._id);
+        draft.list.unshift(action.review);
       });
     default:
       return state;
