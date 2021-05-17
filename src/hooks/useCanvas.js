feb3adb03d575dfc74fb9eab4ebb7deccc7f167d
eluka22 <eluka22@gmail.com>
2021-05-17 18:07:25 +0900

    Fix: clean eventlistener in useCanvas

--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -5,30 +5,38 @@ import Item from "../utils/canvasItem";
 
 const useCanvas = (room, canvasRef, items, runImageAction) => {
   useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const circle = document.getElementById("circle");
+    const drawnItems = [];
+
+    const clickEvent = (event) => {
+      for (const item of drawnItems) {
+        item.runEvent(drawnItems, canvas, context, event, runImageAction);
+      }
+    };
+
+    const mouseMoveEvent = (event) => {
+      runCursorEffect(circle, canvas, event, drawnItems);
+    };
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
     (async () => {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext("2d");
-      const circle = document.getElementById("circle");
-      const drawnItems = [];
-
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
       for (const item of items) {
         const image = new Item(item);
         await image.draw(context);
         drawnItems.push(image);
       }
 
-      circle.addEventListener("click", (event) => {
-        for (const item of drawnItems) {
-          item.runEvent(drawnItems, canvas, context, event, runImageAction);
-        }
-      });
-
-      canvas.addEventListener("mousemove", (event) => {
-        runCursorEffect(circle, canvas, event, drawnItems);
-      });
+      circle.addEventListener("click", clickEvent);
+      canvas.addEventListener("mousemove", mouseMoveEvent);
     })();
+
+    return () => {
+      circle.removeEventListener("click", clickEvent);
+      canvas.removeEventListener("mousemove", mouseMoveEvent);
+    };
   }, [room]);
 };
 
