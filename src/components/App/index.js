4444fd506680533a83524a6abb84816fdf567a7c
eluka22 <eluka22@gmail.com>
2021-05-20 15:39:22 +0900

    Update: check mobile device

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { useSelector } from "react-redux";
 import { Switch, Route } from "react-router-dom";
@@ -12,12 +12,41 @@ import Ending from "../Ending";
 import Game from "../Game";
 import Main from "../Main";
 import Review from "../Review";
+import Mobile from "../Shared/Mobile";
 import NotFound from "../Shared/NotFound";
 import PrivateRoute from "../Shared/PrivateRoute";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
   const playerName = useSelector((state) => state.player.name);
   const playerPassword = useSelector((state) => state.game.playerPassword);
+
+  useEffect(() => {
+    let hasTouchScreen = false;
+
+    if ("maxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.msMaxTouchPoints > 0;
+    } else {
+        const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+        if (mQ && mQ.media === "(pointer:coarse)") {
+            hasTouchScreen = !!mQ.matches;
+        } else if ("orientation" in window) {
+            hasTouchScreen = true;
+        } else {
+            const UA = navigator.userAgent;
+            hasTouchScreen = (
+                /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+            );
+        }
+    }
+
+    if (hasTouchScreen) {
+      setIsMobile(true);
+    }
+  }, []);
 
   return (
     <>
@@ -25,21 +54,25 @@ function App() {
         <GlobalStyles />
         <GlobalFonts />
         <ModalProvider>
-          <Switch>
-            <Route path="/" exact component={Main} />
-            <PrivateRoute
-              path="/game/room/:room"
-              isAuthenticated={Boolean(playerName !== "")}
-              component={Game}
-            />
-            <PrivateRoute
-              path="/game/ending"
-              isAuthenticated={playerPassword["password5"].isUnlocked}
-              component={Ending}
-            />
-            <Route path="/review" component={Review} />
-            <Route render={(props) => <NotFound {...props} title={"404\nNot Found"} text="이런.. 존재하지 않는 페이지입니다." />}/>
-          </Switch>
+          {isMobile
+            ? <Route component={Mobile} />
+            : (
+              <Switch>
+                <Route path="/" exact component={Main} />
+                <PrivateRoute
+                  path="/game/room/:room"
+                  isAuthenticated={Boolean(playerName !== "")}
+                  component={Game}
+                />
+                <PrivateRoute
+                  path="/game/ending"
+                  isAuthenticated={playerPassword["password5"].isUnlocked}
+                  component={Ending}
+                />
+                <Route path="/review" component={Review} />
+                <Route render={(props) => <NotFound {...props} title={"404\nNot Found"} text="이런.. 존재하지 않는 페이지입니다." />}/>
+              </Switch>
+            )}
         </ModalProvider>
       </ThemeProvider>
     </>
